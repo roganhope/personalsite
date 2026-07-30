b9df6d5902d7ee0e1a6e484749025f265579033d
Hope <hope.e.rogan@gmail.com>
2026-07-29 20:17:49 -0400

    Fix plus/minus toggle rotating back to vertical while still hovered

--- src/components/experience-list.tsx
+++ src/components/experience-list.tsx
@@ -27,8 +27,8 @@ export default function ExperienceList() {
               <time className="text-[.76rem] font-[750] whitespace-nowrap">{role.years}</time>
               <span
                 aria-hidden="true"
-                className={`relative h-5 w-5 transition-[color,transform] duration-[220ms] ease-in-out group-hover:rotate-90 group-hover:text-pink before:absolute before:top-1/2 before:left-0 before:h-[1.5px] before:w-full before:-translate-y-1/2 before:bg-current before:content-[''] after:absolute after:top-1/2 after:left-0 after:h-[1.5px] after:w-full after:-translate-y-1/2 after:rotate-90 after:bg-current after:transition-[transform,opacity] after:duration-[280ms] after:ease-in-out after:content-[''] max-[700px]:absolute max-[700px]:top-1/2 max-[700px]:right-6 max-[700px]:-mt-2.5 ${
-                  isOpen ? "text-pink after:scale-x-0 after:opacity-0" : ""
+                className={`relative h-5 w-5 transition-[color,transform] duration-[220ms] ease-in-out group-hover:text-pink before:absolute before:top-1/2 before:left-0 before:h-[1.5px] before:w-full before:-translate-y-1/2 before:bg-current before:content-[''] after:absolute after:top-1/2 after:left-0 after:h-[1.5px] after:w-full after:-translate-y-1/2 after:rotate-90 after:bg-current after:transition-[transform,opacity] after:duration-[280ms] after:ease-in-out after:content-[''] max-[700px]:absolute max-[700px]:top-1/2 max-[700px]:right-6 max-[700px]:-mt-2.5 ${
+                  isOpen ? "text-pink after:scale-x-0 after:opacity-0" : "group-hover:rotate-90"
                 }`}
               />
             </button>
